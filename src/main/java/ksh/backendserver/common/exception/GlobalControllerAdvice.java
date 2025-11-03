408b4b03f0b5ea2df563a7724aa74fc4995a3156
kim-se-hee <saree98@naver.com>
2025-11-03 11:03:45 +0900

    feat: GlobalControllerAdvice 기반 api 예외 처리 구현

---
+++ src/main/java/ksh/backendserver/common/exception/GlobalControllerAdvice.java
@@ -0,0 +1,59 @@
+package ksh.backendserver.common.exception;
+
+import ksh.backendserver.common.dto.response.ApiResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Locale;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+@Slf4j
+public class GlobalControllerAdvice {
+
+    private final MessageSource messageSource;
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ApiResponseDto> handleCustomException(CustomException e, Locale locale) {
+        ErrorCode errorCode = e.getErrorCode();
+        String message = messageSource.getMessage(
+            errorCode.getMessageKey(),
+            e.getMessageArgs().toArray(),
+            locale
+        );
+
+        log.info("예외 정보 : {} {}", errorCode.name(), message);
+
+        ApiResponseDto response = ApiResponseDto.of(
+            errorCode.getStatus(),
+            errorCode.name(),
+            message,
+            null
+        );
+
+        return ResponseEntity
+            .status(errorCode.getStatus())
+            .body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponseDto> handleException(Exception e, Locale locale) {
+        log.info("예외 정보 : {}", e.getMessage());
+
+        ApiResponseDto response = ApiResponseDto.of(
+            HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            HttpStatus.INTERNAL_SERVER_ERROR.name(),
+            e.getMessage(),
+            null
+        );
+
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(response);
+    }
+}
