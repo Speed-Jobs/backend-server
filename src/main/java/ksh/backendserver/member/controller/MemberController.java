460370fc1f8a7e8008ea1da83a1952a575f5b5d2
hwiyoon20010309 <hwiyoon01@naver.com>
2025-11-10 17:01:46 +0900

    feat: 회원가입 api 구현

--- src/main/java/ksh/backendserver/member/controller/MemberController.java
+++ src/main/java/ksh/backendserver/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package ksh.backendserver.member.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import ksh.backendserver.common.dto.request.MemberRegisterRequestDto;
 import ksh.backendserver.common.dto.response.ApiResponseDto;
 import ksh.backendserver.member.dto.request.LoginRequestDto;
 import ksh.backendserver.member.entity.Member;
@@ -21,45 +22,66 @@ public class MemberController {
 
     @PostMapping("/login")
     public ApiResponseDto<Void> login(
-        @Valid @RequestBody LoginRequestDto dto,
-        HttpServletRequest request
+            @Valid @RequestBody LoginRequestDto dto,
+            HttpServletRequest request
     ) {
         Member member = memberService.findLoginMember(
-            dto.getEmail(),
-            dto.getPassword()
+                dto.getEmail(),
+                dto.getPassword()
         );
 
         HttpSession session = request.getSession();
         session.setAttribute("memberId", member.getId());
 
-         return ApiResponseDto.of(
-             HttpStatus.OK.value(),
-             HttpStatus.OK.name(),
-             "로그인 성공",
-             null
-         );
+        return ApiResponseDto.of(
+                HttpStatus.OK.value(),
+                HttpStatus.OK.name(),
+                "로그인 성공",
+                null
+        );
     }
 
     @PostMapping("/logout")
     public ApiResponseDto<Void> logout(HttpServletRequest request) {
         HttpSession session = request.getSession();
 
-        if(session == null) {
+        if (session == null) {
             return ApiResponseDto.of(
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.name(),
-                "로그인 상태가 아닙니다.",
-                null
+                    HttpStatus.BAD_REQUEST.value(),
+                    HttpStatus.BAD_REQUEST.name(),
+                    "로그인 상태가 아닙니다.",
+                    null
             );
         }
 
         session.invalidate();
 
         return ApiResponseDto.of(
-            HttpStatus.OK.value(),
-            HttpStatus.OK.name(),
-            "로그아웃에 성공했습니다.",
-            null
+                HttpStatus.OK.value(),
+                HttpStatus.OK.name(),
+                "로그아웃에 성공했습니다.",
+                null
         );
+    }
+
+
+    @PostMapping("/members")
+    public ApiResponseDto<Void> register(
+            @Valid @RequestBody MemberRegisterRequestDto request
+    ) {
+        memberService.register(
+                request.getName(),
+                request.getEmail(),
+                request.getPassword(),
+                request.getPasswordConfirm()
+        );
+
+        return ApiResponseDto.of(
+                HttpStatus.CREATED.value(),
+                HttpStatus.CREATED.name(),
+                "회원가입이 완료되었습니다.",
+                null
+        );
+
     }
 }
