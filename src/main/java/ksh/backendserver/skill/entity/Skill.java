0fe42b1610b7fd6d59dbf44fe917ff2f94abe2e9
kim-se-hee <saree98@naver.com>
2025-11-10 11:00:49 +0900

    feat: 기술 엔티티 생성

---
+++ src/main/java/ksh/backendserver/skill/entity/Skill.java
@@ -0,0 +1,29 @@
+package ksh.backendserver.skill.entity;
+
+import jakarta.persistence.*;
+import ksh.backendserver.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@SQLDelete(sql = "update skill set is_deleted = true where id = ?")
+@Where(clause = "is_deleted = false")
+public class Skill extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private boolean isDeleted;
+}
