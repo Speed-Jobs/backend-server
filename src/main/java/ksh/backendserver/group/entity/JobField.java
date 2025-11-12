187c855512af47e5b76497370c108ce0dc7e9e9e
kim-se-hee <saree98@naver.com>
2025-11-12 17:46:38 +0900

    feat: 직군 엔티티 추가

---
+++ src/main/java/ksh/backendserver/group/entity/JobField.java
@@ -0,0 +1,31 @@
+package ksh.backendserver.group.entity;
+
+import jakarta.persistence.*;
+import ksh.backendserver.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@SQLDelete(sql = "update job_field set is_deleted = true where id = ?")
+@Where(clause = "is_deleted = false")
+public class JobField extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private boolean isDeleted;
+}
