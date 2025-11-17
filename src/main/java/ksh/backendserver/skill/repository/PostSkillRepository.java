cc79780523d05993f263fa5c73d57e2c17a31708
kim-se-hee <saree98@naver.com>
2025-11-17 23:46:54 +0900

    feat: 일정 기간 동안 공고 수 조회 구현

--- src/main/java/ksh/backendserver/skill/repository/PostSkillRepository.java
+++ src/main/java/ksh/backendserver/skill/repository/PostSkillRepository.java
@@ -3,5 +3,9 @@ package ksh.backendserver.skill.repository;
 import ksh.backendserver.skill.entity.PostSkill;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface PostSkillRepository extends JpaRepository<PostSkill, Long>, PostSkillQueryRepository {
+
+    long countByCreatedAtGreaterThanEqual(LocalDateTime baseTime);
 }
