e05bf4d6f432efd145e0878b606c48129a35c6a0
kim-se-hee <saree98@naver.com>
2025-11-18 11:11:38 +0900

    refactor: 특정 시점 이후 공고 수 조회 쿼리 위치 변경

--- src/main/java/ksh/backendserver/post/repository/PostRepository.java
+++ src/main/java/ksh/backendserver/post/repository/PostRepository.java
@@ -3,5 +3,9 @@ package ksh.backendserver.post.repository;
 import ksh.backendserver.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface PostRepository extends JpaRepository<Post,Long>, PostQueryRepository {
+
+    long countByPostedAtGreaterThanEqual(LocalDateTime baseTime);
 }
