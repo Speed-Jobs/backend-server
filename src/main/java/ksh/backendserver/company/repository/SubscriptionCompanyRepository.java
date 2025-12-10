d5b3dbd1ab7d1c1a005c5a0c4958e4420c434338
kim-se-hee <saree98@naver.com>
2025-12-10 11:49:02 +0900

    feat: 구독 정보 엔티티를 위한 리포지토리 추가

---
+++ src/main/java/ksh/backendserver/company/repository/SubscriptionCompanyRepository.java
@@ -0,0 +1,10 @@
+package ksh.backendserver.company.repository;
+
+import ksh.backendserver.company.entity.SubscriptionCompany;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SubscriptionCompanyRepository extends JpaRepository<SubscriptionCompany, Long> {
+    List<SubscriptionCompany> findByUserId(Long userId);
+}
