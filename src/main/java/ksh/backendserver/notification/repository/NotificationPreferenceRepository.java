5b06d224dac65e67ab24ba4b1daab18ef4576bc7
kim-se-hee <saree98@naver.com>
2025-12-10 15:03:15 +0900

    feat: 사용자 알림 선호 엔티티 리포지토리 추가

---
+++ src/main/java/ksh/backendserver/notification/repository/NotificationPreferenceRepository.java
@@ -0,0 +1,13 @@
+package ksh.backendserver.notification.repository;
+
+import ksh.backendserver.notification.entity.NotificationPreference;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificationPreferenceRepository extends JpaRepository<NotificationPreference, Long> {
+
+    List<NotificationPreference> findByMemberId(Long memberId);
+
+    List<NotificationPreference> findByMemberIdIn(List<Long> memberIds);
+}
