f14ba94fdd765da6bc71aab4304611b07b2c5955
kim-se-hee <saree98@naver.com>
2025-12-16 09:58:43 +0900

    feat: 슬랙 알림 전송 구현

--- src/main/java/ksh/backendserver/skill/repository/PostSkillRepository.java
+++ src/main/java/ksh/backendserver/skill/repository/PostSkillRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface PostSkillRepository extends JpaRepository<PostSkill, Long>, PostSkillQueryRepository {
 
     List<PostSkill> findByPostIdIn(List<Long> postIds);
+
+    List<PostSkill> findByPostId(long postId);
 }
