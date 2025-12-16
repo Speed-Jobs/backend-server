bced6d200fd6487909b750a36e4662db6226825d
kim-se-hee <saree98@naver.com>
2025-12-16 17:43:47 +0900

    refactor: 구독 생성 요청 DTO에서 memberId 제거 - memberId는 세션에서 조회

--- src/main/java/ksh/backendserver/subscription/dto/request/SubscriptionCreationRequestDto.java
+++ src/main/java/ksh/backendserver/subscription/dto/request/SubscriptionCreationRequestDto.java
@@ -1,7 +1,6 @@
 package ksh.backendserver.subscription.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,10 +10,6 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 public class SubscriptionCreationRequestDto {
-
-    @Schema(description = "사용자 ID", example = "1")
-    @NotNull(message = "사용자 ID는 필수입니다.")
-    private Long memberId;
 
     @Schema(description = "구독할 회사 ID 목록", example = "[1, 2, 3]")
     private List<Long> companyIds;
