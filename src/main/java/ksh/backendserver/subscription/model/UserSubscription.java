3b972b1182bb6b0330e446b9b824804921701e7e
kim-se-hee <saree98@naver.com>
2025-12-16 15:45:54 +0900

    refactor: 유저 구독 정보에 직무 id를 직군 id로 변경

--- src/main/java/ksh/backendserver/subscription/model/UserSubscription.java
+++ src/main/java/ksh/backendserver/subscription/model/UserSubscription.java
@@ -1,20 +1,22 @@
 package ksh.backendserver.subscription.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.util.Set;
 
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class UserSubscription {
 
     private Long userId;
     private Set<Long> skillIds;
-    private Set<Long> industryIds;
+    private Set<Long> positionIds;
     private Set<Long> companyIds;
 
-    public static UserSubscription of(Long userId, Set<Long> skillIds, Set<Long> industryIds, Set<Long> companyIds) {
-        return new UserSubscription(userId, skillIds, industryIds, companyIds);
+    public static UserSubscription of(Long userId, Set<Long> skillIds, Set<Long> positionIds, Set<Long> companyIds) {
+        return new UserSubscription(userId, skillIds, positionIds, companyIds);
     }
 }
