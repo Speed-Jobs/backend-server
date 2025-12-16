5aea0ca7437aba556c49593cee5171338dc05f0e
kim-se-hee <saree98@naver.com>
2025-12-16 15:38:43 +0900

    feat: 직군 기반 구독을 위한 SubscriptionPosition 엔티티 추가

---
+++ src/main/java/ksh/backendserver/group/entity/SubscriptionPosition.java
@@ -0,0 +1,31 @@
+package ksh.backendserver.group.entity;
+
+import jakarta.persistence.*;
+import ksh.backendserver.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@SQLDelete(sql = "update subscription_position set is_deleted = true where id = ?")
+@Where(clause = "is_deleted = false")
+public class SubscriptionPosition extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private Long positionId;
+
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private boolean isDeleted;
+}
